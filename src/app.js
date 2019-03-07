f06f15ff65966420f6ccf60c692571c0b29fa384
Aníbal Henrique de Souza <annibalhsouza@gmail.com>
2019-03-07 13:09:57 -0300

    #ISSUE

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'vuetify/dist/vuetify.min.css' 
+import Vuetify from 'vuetify'
 
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 new Vue({
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
     <h1>Home</h1>
+    <v-btn color="success">Success</v-btn>
+   <v-btn color="error">Error</v-btn>
+   <v-btn color="warning">Warning</v-btn>
+   <v-btn color="info">Info</v-btn>
   </div>
 </template>
 
 <style scoped>
+
 h1 {
   color: blue
 }
+
 </style>
 
 <script>
