81b0908fb8379c6a53b23811dcb38b689aeb2463
Aníbal Henrique <annibalhsouza@gmail.com>
2019-03-13 17:47:02 -0300

    #ISSUE Altering columns of Home view

--- src/components/vuetify/Subheader.vue
+++ src/components/vuetify/Subheader.vue
@@ -20,52 +20,6 @@
 
     </v-toolbar>
 
-    <v-container
-      v-for="type in types"
-      :key="type"
-      fluid
-      grid-list-md
-      grey
-      lighten-2
-    >
-
-      <v-layout row wrap>
-        <v-spacer></v-spacer>
-        <v-flex
-          v-for="card in cards"
-          :key="card"
-          xs12
-          sm6
-          md6
-        >
-          <v-card>
-            <v-img
-              :src="`https://picsum.photos/200/300?image=${getImage()}`"
-              height="300px"
-            >
-              <span
-                class="headline white--text pl-3 pt-3"
-                v-text="card.title"
-              ></span>
-            </v-img>
-
-            <v-card-actions class="white justify-center">
-              <v-btn
-                v-for="(social, i) in socials"
-                :key="i"
-                :color="social.color"
-                class="white--text"
-                fab
-                icon
-                small
-              >
-                <v-icon>{{ social.icon }}</v-icon>
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-container>
   </v-card>
 </template>
 
