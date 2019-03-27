69d5f7e2b4f005a6688fbf1203609f2feeb826b4
Aníbal Henrique <annibalhsouza@gmail.com>
2019-03-26 21:25:15 -0300

    #ISSUE Create component stone-btn

--- src/patterns/Buttons/Buttons.stories.js
+++ src/patterns/Buttons/Buttons.stories.js
@@ -2,13 +2,18 @@ import {storiesOf} from '@storybook/vue'
 import {action} from '@storybook/addon-actions'
 import {linkTo} from '@storybook/addon-links'
 
+import StoneBtn from './stoneButton'
+
 import ButtonsV1 from './ButtonsV1'
 import ButtonsV2 from './ButtonsV2'
 import ButtonsV3 from './ButtonsV3'
-import ButtonsV4 from './ButtonsV4'
-import ButtonsV5 from './ButtonsV5'
 
 storiesOf('Buttons', module)
+  .add('StoneBtnV1', () => ({
+    components: {StoneBtn},
+    template: '<stone-btn>STONES</stone-btn>',
+    methods: { action: action('clicked') }
+  }))
   .add('V1', () => ({
     components: {ButtonsV1},
     template: '<ButtonsV1 />'
@@ -20,12 +25,4 @@ storiesOf('Buttons', module)
   .add('V3', () => ({
     components: {ButtonsV3},
     template: '<ButtonsV3 />'
-  }))
-  .add('V4', () => ({
-    components: {ButtonsV4},
-    template: '<ButtonsV4 />'
-  }))
-  .add('V5', () => ({
-    components: {ButtonsV5},
-    template: '<ButtonsV5 />'
   }))
---
+++ src/patterns/Buttons/stoneButton.js
@@ -0,0 +1,72 @@
+export default {
+  name: 'stone-btn',
+  props: ['red', 'blue', 'green', 'white', 'purple', 'pink', 'orange'],
+  data() {
+    return {
+      btn: '',
+      stoneButton: {
+        boxSizing: 'border-box',
+        webkitAppearance: 'none',
+        mozAppearance: 'none',
+        appearance: 'none',
+        backgroundColor: 'transparent',
+        border: '2px solid #e74c3c',
+        borderRadius: '0.6em',
+        color: '#e74c3c',
+        cursor: 'pointer',
+        display: 'flex',
+        alignSelf: 'center',
+        fontSize: '1rem',
+        fontWeight: '400',
+        lineHeight: '1',
+        margin: '20px',
+        padding: '1.2em 2.8em',
+        textDecoration: 'none',
+        textAlign: 'center',
+        textTransform: 'uppercase',
+        fontFamily: 'Montserrat, sans-serif',
+        fontWeight: '700',
+      },
+      stoneButtonA: {
+        boxSizing: 'border-box',
+        webkitAppearance: 'none',
+        mozAppearance: 'none',
+        appearance: 'none',
+        backgroundColor: 'transparent',
+        border: '2px solid #fff',
+        borderRadius: '0.6em',
+        color: '#fff',
+        cursor: 'pointer',
+        display: 'flex',
+        alignSelf: 'center',
+        fontSize: '1rem',
+        fontWeight: '400',
+        lineHeight: '1',
+        margin: '20px',
+        padding: '1.2em 2.8em',
+        textDecoration: 'none',
+        textAlign: 'center',
+        textTransform: 'uppercase',
+        fontFamily: 'Montserrat, sans-serif',
+        fontWeight: '700',
+      }
+    }
+  },
+  template: `
+    <button v-on:mouseover="mouseover" v-on:mouseleave="mouseleave" :style="stoneButtonA" @click="onClick">
+      <slot />
+    </button>
+  `,
+  methods: {
+    onClick() {
+      this.$emit('click')
+    },
+    mouseover: function(){
+      this.btn = 'A'
+     },
+     mouseleave: function(){
+       this.btn = ''
+     }
+  }
+
+}
